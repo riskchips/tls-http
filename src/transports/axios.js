77f865e566dfc957a82c2df5c6568871c37a1276
riskchips69 <arnabdas91das@gmail.com>
2026-05-17 14:14:42 +0530

    fix issue

--- src/transports/axios.js
+++ src/transports/axios.js
@@ -1,80 +1,44 @@
 ﻿const axios = require('axios')
 const { wrapper } = require('axios-cookiejar-support')
 
-const http = require('http')
-const https = require('https')
-
-const { HttpsProxyAgent } = require('https-proxy-agent')
-const { HttpProxyAgent } = require('http-proxy-agent')
-
-function buildProxyAgent(proxy = {}) {
-  if (!proxy.host || !proxy.port) {
-    return null
-  }
-
-  const protocol = proxy.protocol || 'http'
-
-  let auth = ''
-
-  if (proxy.username && proxy.password) {
-    auth = `${proxy.username}:${proxy.password}@`
-  }
-
-  const proxyUrl =
-    `${protocol}://${auth}${proxy.host}:${proxy.port}`
-
-  return {
-    httpAgent: new HttpProxyAgent(proxyUrl),
-    httpsAgent: new HttpsProxyAgent(proxyUrl)
-  }
-}
-
 function createAxiosInstance({
   jar,
   timeout = 30000,
   proxy
 } = {}) {
 
-  const keepAliveHttpAgent = new http.Agent({
-    keepAlive: true,
-    keepAliveMsecs: 30000,
-    maxSockets: 128,
-    maxFreeSockets: 32,
-    scheduling: 'lifo'
-  })
-
-  const keepAliveHttpsAgent = new https.Agent({
-    keepAlive: true,
-    keepAliveMsecs: 30000,
-    maxSockets: 128,
-    maxFreeSockets: 32,
-    scheduling: 'lifo'
-  })
-
   const config = {
     jar,
     withCredentials: true,
+
     timeout,
+
     maxRedirects: 5,
+
     decompress: true,
+
     validateStatus: () => true,
 
     headers: {
       'connection': 'keep-alive'
-    },
-
-    httpAgent: keepAliveHttpAgent,
-    httpsAgent: keepAliveHttpsAgent
+    }
   }
 
   if (proxy) {
-    const proxyAgents = buildProxyAgent(proxy)
+    config.proxy = {
+      host: proxy.host,
+      port: proxy.port,
+      protocol: proxy.protocol || 'http'
+    }
 
-    if (proxyAgents) {
-      config.httpAgent = proxyAgents.httpAgent
-      config.httpsAgent = proxyAgents.httpsAgent
-
-      config.proxy = false
+    if (
+      proxy.username &&
+      proxy.password
+    ) {
+      config.proxy.auth = {
+        username: proxy.username,
+        password: proxy.password
+      }
     }
   }
 
@@ -82,28 +46,33 @@ function createAxiosInstance({
     axios.create(config)
   )
 
-  instance.interceptors.request.use((request) => {
+  instance.interceptors.request.use(
+    (request) => {
 
-    delete request.transport
+      delete request.transport
 
-    delete request.fingerprint
+      delete request.fingerprint
 
-    if (
-      request.headers &&
-      !request.headers['accept-encoding']
-    ) {
-      request.headers['accept-encoding'] =
-        'gzip, deflate, br'
+      if (
+        request.headers &&
+        !request.headers['accept-encoding']
+      ) {
+        request.headers['accept-encoding'] =
+          'gzip, deflate, br'
+      }
+
+      return request
     }
-
-    return request
-  })
+  )
 
   instance.interceptors.response.use(
     (response) => response,
 
     (error) => {
-      if (error.code === 'ECONNABORTED') {
+
+      if (
+        error.code === 'ECONNABORTED'
+      ) {
         error.name = 'TimeoutError'
       }
 
